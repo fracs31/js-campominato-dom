94ba93241f7f1acc377dd939fbd23d9d72ca01e6
fracs31 <pist.fracs95@gmail.com>
2023-02-10 15:12:45 +0100

    TRYING TO FIX the bomb cells

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let buttonElement = document.getElementById("btn"); //bottone per iniziare la partita
 let difficultyElement = document.getElementById("difficulty"); //select per la scelta della difficoltà
+
 buttonElement.addEventListener("click", startGame); //quando viene premuto il bottone il gioco inizia
 
 //Funzione per avviare il gioco
@@ -8,10 +9,8 @@ function startGame() {
     let cells = side ** 2 ; //numero di celle
     let bombs = []; //celle con le bombe
     let gridElement = document.getElementById("grid"); //griglia nel DOM
-
     buttonElement.remove(); //elimino il bottone per iniziare la partita
     difficultyElement.remove(); //elimino la scelta della difficoltà
-
     //Ciclo per la creazione delle celle della griglia
     for (let i = 0; i < cells; i++) {
         let cellElement = document.createElement("div"); //creazione della cella
@@ -22,7 +21,7 @@ function startGame() {
         cellElement.innerHTML = value; //assegno alla cella il valore
         gridElement.appendChild(cellElement); //aggiungo la cella alla griglia
     }
-    //Crezione delle bombe
+    //Creazione delle bombe
     for (let i = 0; i < 16; i++) {
         let random = getRandomIntInclusive(0, (cells - 1)); //creo il numero random
         //Se il numero random non è presente dentro l'array di bombe
@@ -32,6 +31,7 @@ function startGame() {
             i--; //ripeto l'operazione
         }
     }
+    console.log(bombs);
 }
 
 //Funzione per quando viene cliccata la cella
@@ -40,11 +40,11 @@ function onClick() {
     let position = parseInt(this.innerHTML) - 1; //posizione della cella
     console.log(cell);
     console.log(position);
-    console.log(bombs);
+    //console.log(bombs);
     //Se la cella cliccata è una bomba
     if (bombs.find(element => element == position)) {
         cell.classList.add("bg-bomb"); //cambio la classe alla cella cliccata
-        
+        cell.removeEventListener("click", onClick); //elimino l'event listener
     } else { //altrimenti
         cell.classList.add("bg-safe"); //cambio la classe alla cella cliccata
     }
