37ce5d18096cac710c404711c4c98c3e30bba86f
fracs31 <pist.fracs95@gmail.com>
2023-02-10 17:29:38 +0100

    ADD end of the game when you click all the safe cell

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 let buttonElement = document.getElementById("btn"); //bottone per iniziare la partita
 let difficultyElement = document.getElementById("difficulty"); //select per la scelta della difficoltà
 let bombs = []; //celle con le bombe
-
+let safes = []; //celle sicure
+let checkSafe = 0; //indice per verificare se tutte le celle sicure sono state cliccate
 buttonElement.addEventListener("click", startGame); //quando viene premuto il bottone il gioco inizia
 
 //Funzione per avviare il gioco
@@ -31,17 +32,22 @@ function startGame() {
             i--; //ripeto l'operazione
         }
     }
-    console.log(bombs);
+    //Salvataggio celle sicure
+    for (let i = 0; i < cells; i++) {
+        //Se il la posizione della cella non è presente dentro l'array di bombe
+        if (!bombs.includes(i)) {
+            safes.push(i); //inserisco il numero nell'array di celle sicure
+        }
+    }
+    console.log(bombs); //stampo in console l'array di bombe
+    console.log(safes); //stampo in console l'array di celle sicure
 }
 
 //Funzione per quando viene cliccata la cella
 function onClick() {
     let cell = this; //cella cliccata
     let position = parseInt(this.innerHTML) - 1; //posizione della cella
-    let cellsElement = document.querySelectorAll(".cell");
-    console.log(cell);
-    console.log(position);
-    //console.log(bombs);
+    let cellsElement = document.querySelectorAll(".cell"); //celle nel DOM
     //Se la cella cliccata è una bomba
     if (bombs.includes(position)) {
         cell.classList.add("bg-bomb"); //cambio il background color in rosso
@@ -49,8 +55,17 @@ function onClick() {
         for (let i = 0; i < cellsElement.length; i++) {
             cellsElement[i].classList.add("not-clickable"); //aggiundo la classe per non far cliccare le celle
         }
-    } else { //altrimenti
+    } else { //altrimenti 
         cell.classList.add("bg-safe"); //cambio il background color in azzurro
+        checkSafe++; //incremento l'indice di celle sicure cliccate
+        console.log(checkSafe); //stampo il console l'indice di celle sicure cliccate
+        //Se tutte le celle sicure sono state cliccate
+        if (safes.checkSafe == safes.length){
+            //Rendo tutte le celle non cliccabili
+            for (let i = 0; i < cellsElement.length; i++) {
+                cellsElement[i].classList.add("not-clickable"); //aggiundo la classe per non far cliccare le celle
+            }
+        }
     }
 }
 
