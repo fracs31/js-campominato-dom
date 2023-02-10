08da52d2fbaf4b4b917b94b05acae227a2997c47
fracs31 <pist.fracs95@gmail.com>
2023-02-10 14:47:39 +0100

    ADD difficulty

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 let buttonElement = document.getElementById("btn"); //bottone per iniziare la partita
-
+let difficultyElement = document.getElementById("difficulty"); //select per la scelta della difficoltà
 buttonElement.addEventListener("click", startGame); //quando viene premuto il bottone il gioco inizia
 
+//Funzione per avviare il gioco
 function startGame() {
-    buttonElement.remove(); //elimino il bottone per iniziare la partita
-    let side = 10; //lato della griglia
+    let side = parseInt(difficultyElement.value); //lato della griglia
     let cells = side ** 2 ; //numero di celle
     let bombs = []; //celle con le bombe
     let gridElement = document.getElementById("grid"); //griglia nel DOM
+
+    buttonElement.remove(); //elimino il bottone per iniziare la partita
+    difficultyElement.remove(); //elimino la scelta della difficoltà
+
     //Ciclo per la creazione delle celle della griglia
     for (let i = 0; i < cells; i++) {
         let cellElement = document.createElement("div"); //creazione della cella
@@ -31,7 +35,7 @@ function startGame() {
 }
 
 //Funzione per quando viene cliccata la cella
-function onClick(bombs) {
+function onClick() {
     let cell = this; //cella cliccata
     let position = parseInt(this.innerHTML) - 1; //posizione della cella
     console.log(cell);
@@ -40,13 +44,15 @@ function onClick(bombs) {
     //Se la cella cliccata è una bomba
     if (bombs.find(element => element == position)) {
         cell.classList.add("bg-bomb"); //cambio la classe alla cella cliccata
+        
     } else { //altrimenti
         cell.classList.add("bg-safe"); //cambio la classe alla cella cliccata
     }
 }
 
+//Funzione per ottenere dei numeri causali
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}  
+    min = Math.ceil(min); //arrotondo per eccesso
+    max = Math.floor(max); //arrotondo per difetto
+    return Math.floor(Math.random() * (max - min + 1) + min); //restituisco il numero casuale
+}
